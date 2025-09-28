043b9cd00727c819bb8a6e4cea7c65a35058bc76
Luis Machuca <n00420048@upn.pe>
2025-09-28 11:57:48 -0500

    PracticaCampoS5

---
+++ Practica_Campo_S5/Practica_Campo_S5/LibreriaMensajes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_Campo_S5
+{
+    class LibreriaMensajes
+    {
+        static public void menuInventario()
+        {
+            Console.Clear();
+            Console.WriteLine("======== MENÚ INVENTARIO =======");
+            Console.WriteLine("1. Agregar producto");
+            Console.WriteLine("2. Consultar productos disponibles");
+            Console.WriteLine("3. Actualizar stock");
+            Console.WriteLine("4. Eliminar producto");
+            Console.WriteLine("5. Salir");
+            Console.WriteLine("================================");
+        }
+        static public void mensajeInvalido()
+        {
+            Console.WriteLine("Opción Invalida. Intente de nuevo");
+        }
+
+        static public void mensajeparaContinuar()
+        {
+            Console.WriteLine("\nPresione una tecla para continuar ....");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Practica_Campo_S5/Practica_Campo_S5/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_Campo_S5
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int opcionesmenu;
+
+            do
+            {
+                LibreriaMensajes.menuInventario();
+                Console.Write("Seleccione una opción: ");
+                opcionesmenu = int.Parse(Console.ReadLine());
+
+                if (opcionesmenu >= 1 && opcionesmenu <= 4)
+                {
+                    switch (opcionesmenu)
+                    {
+                        case 1:
+
+                            break;
+                        case 2:
+
+                            break;
+                        case 3:
+
+                            break;  
+                        case 4: 
+
+                            break;
+                    }
+                }
+                else
+                {
+                    if (opcionesmenu != 5)
+                    {
+                        LibreriaMensajes.mensajeInvalido();
+                    }
+
+                    LibreriaMensajes.mensajeparaContinuar();
+                }
+            }
+            while (opcionesmenu != 5);
+        }
+    }
+}
